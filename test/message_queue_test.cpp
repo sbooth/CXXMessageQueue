07ad4e5e6b0acfdb3dc3df72a25245bef8a070aa
Stephen Booth <me@sbooth.org>
2026-07-06 16:40:18 -0600

    Update test

--- test/message_queue_test.cpp
+++ test/message_queue_test.cpp
@@ -22,25 +22,24 @@
 
 namespace {
 
-constexpr std::size_t KB = 1024;
-constexpr std::size_t MB = 1024 * KB;
-constexpr std::size_t GB = 1024 * MB;
+constexpr std::size_t slotCount = 32;
+constexpr std::size_t slotCapacity = 32;
 
 class MessageQueueTest : public ::testing::Test {
   protected:
-    mpsc::MessageQueue<32, 512> q;
+    mpsc::MessageQueue<slotCount, slotCapacity> q;
 };
 
 } // namespace
 
-TEST_F(MessageQueueTest, Empty) {
-    EXPECT_EQ(q.slotCount(), 0);
-    EXPECT_EQ(q.emptySlots(), 0);
+TEST_F(MessageQueueTest, Basic) {
+    EXPECT_EQ(q.slotCount(), slotCount);
+    EXPECT_EQ(q.emptySlots(), slotCount);
     EXPECT_EQ(q.occupiedSlots(), 0);
 
-    std::array<unsigned char, 512> a;
+    std::array<unsigned char, slotCapacity> a;
     std::size_t sz;
     EXPECT_EQ(q.dequeue(a, sz), false);
     EXPECT_EQ(sz, 0);
-    EXPECT_EQ(q.enqueue(a), false);
+    EXPECT_EQ(q.enqueue(a), true);
 }
